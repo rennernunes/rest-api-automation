7e39c20fc13bcde940bf51537311b6f4fd2603c4
Renner Nunes <renner.nunes@evoluservices.com>
2022-03-10 20:26:40 -0300

    HelloWorld

---
+++ README.md
@@ -0,0 +1,13 @@
+https://www.google.com.br/search?q=testes+de+api
+
+Protocolo: https
+Endereço: www.google.com.br
+Recurso: search?q=testes+de+api
+Porta padrão http: 80
+Porta padrão https: 443
+
+Ex com URL porta:
+
+https://www.google.com.br:443/search?q=testes+de+api
+
+Fazer uma Requisição retorna um objeto do tipo response que possui um corpo (BODY), cabeçalho (STATUS CODE)
---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    String url = "http://restapi.wcaquino.me/ola";
+    String url_google = "https://www.google.com.br:443/search?q=testes+de+api";
+    
+    Response response = io.restassured.RestAssured.request(Method.GET, url_google);
+    System.out.println(response.getBody().asString());
+  }
+}
