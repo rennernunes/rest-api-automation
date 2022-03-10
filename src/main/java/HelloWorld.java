115cdeca97c0e295ed042c434ff203dac8d57a7d
Renner Nunes <renner.nunes@evoluservices.com>
2022-03-10 20:39:32 -0300

    HelloWorld

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,8 +6,9 @@ public class HelloWorld {
   public static void main(String[] args) {
     String url = "http://restapi.wcaquino.me/ola";
     String url_google = "https://www.google.com.br:443/search?q=testes+de+api";
-    
+
     Response response = io.restassured.RestAssured.request(Method.GET, url_google);
     System.out.println(response.getBody().asString());
+    
   }
 }
