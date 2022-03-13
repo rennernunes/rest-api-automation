dfc8c0a049c11d761e55acb28a9e91f6dcf584a6
Renner Nunes <renner.nunes@evoluservices.com>
2022-03-12 22:45:52 -0300

    User Json Other Forms

--- src/main/java/UserJsonTest.java
+++ src/main/java/UserJsonTest.java
@@ -1,9 +1,15 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
+
 
 public class UserJsonTest {
   final String url_fl = "https://restapi.wcaquino.me/users/1";
+  private JsonPath jsonPath;
 
   @Test
   public void firstLevelJson() {
@@ -15,5 +21,21 @@ public class UserJsonTest {
         .body("id", Matchers.is(1))
         .body("name", Matchers.containsString("Silva"))
         .body("age", Matchers.greaterThan(18));
+  }
+
+  @Test
+  public void firstLevelJsonOtherForms() {
+    Response response = RestAssured.request(Method.GET, url_fl);
+
+    //path
+    Assert.assertEquals(new Integer(1), response.path("id"));
+
+    //jsonpath
+    JsonPath jsonPath = new JsonPath(response.asString());
+    Assert.assertEquals(1, jsonPath.getInt("id"));
+
+    //from
+    int id = JsonPath.from(response.asString()).getInt("id");
+    Assert.assertEquals(1, id);
   }
 }
